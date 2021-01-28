8097c2dba7810e2034b72e28a5339aa98a62af2f
pi-simple-adblocker <77189139+pi-simple-adblocker@users.noreply.github.com>
2021-01-28 17:19:38 +0700

    update ublox_gps.py
    add parser HPPOSECEF

--- ublox_gps/ublox_gps.py
+++ ublox_gps/ublox_gps.py
@@ -217,6 +217,21 @@ class UbloxGps(object):
         s_payload = self.scale_NAV_HPPOSLLH(payload)
         return s_payload
 
+    def hpecef_geo_coords(self):
+        """
+        Sends a poll request for the NAV class with the HPPOSLLH Message ID and
+        parses ublox messages for the response. The payload is extracted from
+        the response which is then passed to the user.
+
+        :return: The payload of the NAV Class and HPPOSLLH Message ID
+        :rtype: namedtuple
+        """
+        self.send_message(sp.NAV_CLS, self.nav_ms.get('HPPOSECEF'))
+        parse_tool = core.Parser([sp.NAV_CLS])
+        cls_name, msg_name, payload = parse_tool.receive_from(self.hard_port)
+        s_payload = self.scale_NAV_HPPOSECEF(payload)
+        return s_payload    
+    
     def date_time(self):
         """
         Sends a poll request for the NAV class with the PVT Message ID and
